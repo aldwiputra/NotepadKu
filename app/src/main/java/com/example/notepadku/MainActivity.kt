c01c50bfe5c903d19b19d873ea2b956786e988c3
Aldiansyah <skullpreach@gmail.com>
2022-11-02 09:23:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/notepadku/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.notepadku
+
+import com.example.notepadku.base.BaseActivity
+import com.example.notepadku.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+}
